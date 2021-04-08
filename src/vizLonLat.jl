7755da18960498d30cc7edc2855e11964d62b29d
lazarusA <lazarus.alon@gmail.com>
2021-04-08 18:01:51 +0200

    updated plot function for lon, lat, time variables.

--- src/vizLonLat.jl
+++ src/vizLonLat.jl
@@ -4,19 +4,25 @@ plot(dataset; args...)
 dataset is a Cube from YAXArrays
 
 variable :: the variable to plot
+
 x        :: horizontal axis values, i.e. longitude
+
 y        :: vertical axis values, i.e. latitude
-srcESDL  :: source of data, if its not from ESDL set to false
+
+reversey  :: if the data is reverse in the y direction
+
 cmap     :: colormap
+
 rmvalue  :: remove an specific value
+utime    :: your time selection, format: Date("xxxx-xx-xx")
 
 Returns :
 
 fig, ax, pltobj = plot(...)
 
 """
-function plot(dataset; x = :lon, y = :lat, variable = nothing, legcbar=" ",
-    cmap = :plasma, figsize = (1240,650), rmvalue =nothing, srcESDL = true)
+function plot(dataset::YAXArray; x = :lon, y = :lat, variable = nothing, legcbar=" ",
+    cmap = :plasma, figsize = (1240,650), utime = nothing, rmvalue =nothing, reversey = true)
 
     #steplonticks = nothing, steplatticks = nothing
 
@@ -30,8 +36,23 @@ function plot(dataset; x = :lon, y = :lat, variable = nothing, legcbar=" ",
         label = variable == nothing ? legcbar : string(variable)
     else
         timerange = getAxis("time", dataset).values
-        data = dataset[var = string(variable), time = Date(timerange[1])].data[:,:] # add keyword for user selection of time
-        label = string(variable) * "  $(Date(timerange[1]))"
+        if variable != nothing
+            if utime != nothing
+                data = dataset[var = string(variable), time = utime].data[:,:]
+                label = string(variable) * "  $(utime)"
+            else
+                data = dataset[var = string(variable), time = Date(timerange[1])].data[:,:] # add keyword for user selection of time
+                label = string(variable) * "  $(Date(timerange[1]))"
+            end
+        else
+            if utime != nothing
+                data = dataset[time = utime].data[:,:]
+                label = legcbar * "  $(utime)"
+            else
+                data = dataset[time = Date(timerange[1])].data[:,:] # add keyword for user selection of time
+                label = legcbar * "  $(Date(timerange[1]))"
+            end
+        end
     end
 
     minlon  = round(minimum(lon), RoundDown)
@@ -48,14 +69,14 @@ function plot(dataset; x = :lon, y = :lat, variable = nothing, legcbar=" ",
     if rmvalue != nothing
         data = rmvalue in data ? replace(data, rmvalue=>NaN) : data
     end
-    data = srcESDL == true ? data[:,end:-1:1] : data
+    data = reversey == true ? data[:,end:-1:1] : data
 
     fig = Figure(resolution = figsize, fontsize = 20)
     ax = Axis(fig, xlabel = string(x), ylabel = string(y),
         xtickalign = 1, ytickalign = 1, spinewidth = 0.75)
 
     pltobj = heatmap!(ax, lon, lat, data, colormap = cmap)
-    cbar = Colorbar(fig, plotobject, label = label, width = 11, ticklabelsize =20,
+    cbar = Colorbar(fig, pltobj, label = label, width = 11, ticklabelsize =20,
         tickalign = 1, spinewidth =0.5)
     limits!(ax, minlon, maxlon, minlat, maxlat)
     hidespines!(ax, :t, :r)
